2541f268d5be472d68e21f8795c54043e8d229d1
101214093 <62094835+101214093@users.noreply.github.com>
2020-04-25 21:10:54 +0800

    Add files via upload

---
+++ Snake/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class User
+    {
+        private string name;
+        private int score;
+        public User(string username) {
+            name = username;
+            score = 0;
+        }
+
+        public void ScoreIncrement(int up) {
+            score += up;
+        }
+
+        public string getName
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int getScore {
+            get { return score; }
+            set { score = value; }
+        }
+    }
+}
---
+++ Snake/UserManagement.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{    
+    class UserManagement
+    {
+        string name;
+        int score;
+        private List<User> userlist;
+        public UserManagement() {
+            userlist = new List<User>();
+        }
+
+        public void AddUser(User user) {
+            userlist.Add(user);
+        }
+
+        public void readRecord()
+        {
+            var path = "userRecord.txt";
+            using (StreamReader file = new StreamReader(path))
+            {
+                string ln;
+                while ((ln=file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    ln = file.ReadLine();
+                }
+            }
+        }
+
+        public void recordUser() {
+            var path = "userRecord.txt";
+            StreamWriter sw = File.AppendText(path);
+            foreach (User user in userlist) {
+                //using (StreamWriter sw = File.AppendText(path)) { sw.WriteLine(user.getName+'\t'+user.getScore.ToString()); }
+                sw.WriteLine(user.getName + '\t' + user.getScore.ToString());
+            }
+            sw.Close();
+        }
+
+        public List<User> getUsers
+        {
+            get { return userlist; }
+        }
+    }
+}
